daa900509e4226de41e324c4c216fd87636cf5b5
nilpeep <niluferk038@gmail.com>
2024-05-05 23:47:05 +0300

    dimension destruction

--- OOP/index.js
+++ OOP/index.js
@@ -6,3 +6,14 @@ const matrix = [
     [5,8]
 ]
 
+let dimensionCount = 3;
+
+let dimensionalArray = new Array(dimensionCount);
+
+for (let i = 0; i < 3; i++) {
+    dimensionalArray[i] = new Array(3);
+}
+
+// Diziyi ekrana yazdırma
+console.log(dimensionalArray);
+
