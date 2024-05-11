c10b05ac16531012972fec8662f1b2f0a3f30e17
nilpeep <niluferk038@gmail.com>
2024-05-11 22:46:03 +0300

    up

--- OOP/index.js
+++ OOP/index.js
@@ -1,33 +1,61 @@
-// MATRIX:
+// // MATRIX:
 
 const matrix = [
-    [9,3],
+    [9,4],
     [6,3],
     [5,8]
 ]
 
-function adjacentMatrix(matrix){
-
-    if (Array.isArray(arr)) {
-        let dimensions = 1;
-        let innerArray = arr;
-        while (Array.isArray(innerArray[0])) {
-            dimensions++;
-            innerArray = innerArray[0];
-        }
-    } 
 
 
-    let dimensionCount = 2;
+const getSiblings = (arr) =>{
     
-    let dimensionalArray = new Array(dimensionCount);
-    
-    for (let i = 0; i < dimensionCount; i++) {
-        dimensionalArray[i] = new Array(dimensionCount);
-    }
-    
-    // Diziyi ekrana yazdırma
-    console.log(dimensionalArray);
+    arr.map((item,i)=>{
+        arr[i].map((item,j) =>{
+            const siblings = []
+            console.log(arr[i][j])
+            console.log(i,j)
+            arr[i+1][j] && siblings.push(arr[i+1][j])
+            arr[i-1][j] && siblings.push(arr[i-1][j])
+            arr[i][j+1] && siblings.push(arr[i][j+1])
+            arr[i,j-1] && siblings.push(arr[i][j-1])
+        })
+    } )
 }
 
+getSiblings(matrix)
+
+// class siblings extends Object{
+//     constructor(arr){
+//         this.arr = arr;
+//         this.resultArray = [ ]
+//         for(let i = 0; i<this.count; i++){
+//             let row = []
+
+//             for(let j = 0; j<this.count;j++){
+//                 row.push(0)
+
+//             }
+
+//             this.resultArray.push(row)
+//         }
+//         let x = 0
+//     }
+
+//     getCell (x,y){
+//         return this.arr[x][y]
+//     }
+
+//     get count(){
+//         let result = 0
+//         this.arr.map((i) => result += i.length)
+//         return result
+//     }
+
+//     getIndexCell(index){
+
+//     }
+// }
+
+// new siblings(matrix)
 
