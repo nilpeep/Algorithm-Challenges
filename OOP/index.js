6d36c6969be5b0ddb240a7a5bd6397d16e6b480b
nilpeep <niluferk038@gmail.com>
2024-05-12 22:53:27 +0300

    solution expert

--- OOP/index.js
+++ OOP/index.js
@@ -8,30 +8,45 @@ const matrix = [
 
 const getSiblings = (arr) => {
   const result = [];
-  const gridMatrix = []
+  const gridMatrix = [undefined];
   arr.map((item, i) => {
     arr[i].map((item, j) => {
-      gridMatrix.push(item)
+      gridMatrix.push(item);
       const siblings = [];
       if (arr[i + 1] && arr[i + 1][j]) siblings.push(arr[i + 1][j]);
       if (arr[i - 1] && arr[i - 1][j]) siblings.push(arr[i - 1][j]);
       if (arr[i][j + 1]) siblings.push(arr[i][j + 1]);
       if (arr[i][j - 1]) siblings.push(arr[i][j - 1]);
 
-      result.push(siblings)
+      result.push(siblings);
     });
   });
 
-  const grid = []
+  const grid = [];
 
+  grid.push(gridMatrix);
 
+  gridMatrix.map((item, i) => {
+    const values = [];
+    if (i > 0) {
 
-  return result,gridMatrix;
+      gridMatrix.map((check, j) => {
+        if (j > 0) {
+          if (result[i - 1].includes(check)) {
+            values.push(1);
+          } else {
+            values.push(0);
+          }
+        }
+      });
+      grid.push([item, ...values]);
+    }
+  });
+
+  return grid;
 };
 
-
-
-console.log(getSiblings(matrix))
+console.log(getSiblings(matrix));
 
 // class siblings extends Object{
 //     constructor(arr){
